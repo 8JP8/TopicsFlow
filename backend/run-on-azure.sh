4e17c8b209b2aaad1eb5c94a38bff1bf07218c83
João Oliveira <1240369@isep.ipp.pt>
2025-12-21 02:09:42 +0000

    azure backend fix

--- backend/run-on-azure.sh
+++ backend/run-on-azure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --worker-class eventlet -w 1 --bind=0.0.0.0:8000 --timeout 230 --access-logfile - --error-logfile - app:app
+gunicorn --worker-class eventlet -w 1 --bind=0.0.0.0:8000 --timeout 230 --access-logfile - --error-logfile - app:appp
